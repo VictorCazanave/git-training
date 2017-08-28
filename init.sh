fae0fb05be8e9a60051c505df0f7236c1f1f4753
Victor Cazanave <victor.cazanave@gmail.com>
2017-08-29 04:52:13 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo Creating all branches...
+git checkout -t remotes/origin/ex01
+git checkout -t remotes/origin/ex02
+git checkout -t remotes/origin/ex03
+git checkout -t remotes/origin/ex04
+git checkout -t remotes/origin/ex05
+git checkout -t remotes/origin/ex06
+git checkout -t remotes/origin/ex07
+git checkout -t remotes/origin/ex08
+git checkout master
+echo All branches created.
